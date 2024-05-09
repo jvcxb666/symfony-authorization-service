55d32338cd9e3465d451c51f4245c76e08e5f1ed
Jacob <yakoff.swidersky@gmail.com>
2024-05-09 15:11:33 +0300

    models upd

--- migrations/Version20240509120033.php
+++ migrations/Version20240509120033.php
@@ -21,6 +21,9 @@ final class Version20240509120033 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE "user" (id UUID NOT NULL, first_name VARCHAR(255) DEFAULT NULL, last_name VARCHAR(255) DEFAULT NULL, middle_name VARCHAR(255) DEFAULT NULL, username VARCHAR(255) NOT NULL, email VARCHAR(255) DEFAULT NULL, phone VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX user_phone ON "user" (phone)');
+        $this->addSql('CREATE INDEX user_email ON "user" (email)');
+        $this->addSql('CREATE INDEX user_username ON "user" (username)');
     }
 
     public function down(Schema $schema): void
--- migrations/Version20240509120556.php
+++ migrations/Version20240509120556.php
@@ -21,6 +21,7 @@ final class Version20240509120556 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE token (id UUID NOT NULL, value VARCHAR(255) NOT NULL, created TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, expired TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX token_value ON token (value)');
     }
 
     public function down(Schema $schema): void
