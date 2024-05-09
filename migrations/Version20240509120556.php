71fd05c53a52d7e3718236ab0f2b2d0c1dcf0ff1
Jacob <yakoff.swidersky@gmail.com>
2024-05-09 15:41:45 +0300

    token upd

--- migrations/Version20240509120556.php
+++ migrations/Version20240509120556.php
@@ -20,8 +20,10 @@ final class Version20240509120556 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE token (id UUID NOT NULL, value VARCHAR(255) NOT NULL, created TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, expired TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE token (id UUID NOT NULL, user_id VARCHAR(255) NOT NULL, value VARCHAR(255) NOT NULL, refresh VARCHAR(255) NOT NULL, created TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, expired TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, refresh_expired TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX token_value ON token (value)');
+        $this->addSql('CREATE INDEX token_refresh ON token (refresh)');
+        $this->addSql('CREATE INDEX token_user_id ON token (user_id)');
     }
 
     public function down(Schema $schema): void
--- src/Entity/Token.php
+++ src/Entity/Token.php
@@ -17,13 +17,22 @@ class Token
     private ?string $id = null;
 
     #[ORM\Column(length: 255)]
+    private ?string $user_id = null;
+
+    #[ORM\Column(length: 255)]
     private ?string $value = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $refresh = null;
 
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
     private ?\DateTimeInterface $created = null;
 
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
     private ?\DateTimeInterface $expired = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $refresh_expired = null;
 
     public function getId(): ?string
     {
@@ -62,6 +71,42 @@ class Token
     public function setExpired(\DateTimeInterface $expired): static
     {
         $this->expired = $expired;
+
+        return $this;
+    }
+
+    public function getRefresh(): ?string
+    {
+        return $this->refresh;
+    }
+
+    public function setRefresh(string $refresh): static
+    {
+        $this->refresh = $refresh;
+
+        return $this;
+    }
+
+    public function getRefreshExpired(): ?\DateTimeInterface
+    {
+        return $this->refresh_expired;
+    }
+
+    public function setRefreshExpired(\DateTimeInterface $refresh_expired): static
+    {
+        $this->refresh_expired = $refresh_expired;
+
+        return $this;
+    }
+
+    public function getUserId(): ?string
+    {
+        return $this->user_id;
+    }
+
+    public function setUserId(string $user_id): static
+    {
+        $this->user_id = $user_id;
 
         return $this;
     }
