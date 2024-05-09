bdcaaf0113957dc19cb1fe71e2c779d54a554c1d
Jacob <yakoff.swidersky@gmail.com>
2024-05-09 17:13:31 +0300

    interface upd

--- src/Interface/AuthorizationServiceInterface.php
+++ src/Interface/AuthorizationServiceInterface.php
@@ -4,6 +4,5 @@ namespace App\Interface;
 
 interface AuthorizationServiceInterface extends ServiceInterface
 {
-    public function login(): bool|string;
-    public function logout(): void;
+    public function login(array $request): bool|string;
 }
